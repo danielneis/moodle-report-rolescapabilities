303df34187a8d9a6edc31df7fc54d4c211d2c4f6
Daniel Neis Araujo <daniel@npd.ufsc.br>
2010-07-06 15:33:46 -0300

    CONTRIB-2178 Implementing the rolescapabilities as an extension of capability_table_base

--- index.php
+++ index.php
@@ -30,10 +30,42 @@ echo '<div id="legend_container">',
        '</dl>',
      '</div>';
 
+$sql = "SELECT id, name
+          FROM {$CFG->prefix}role
+         WHERE id IN ({$CFG->report_rolescapabilities_available_roles})
+      ORDER BY sortorder ASC";
+$available_roles = $DB->get_records_sql($sql);
+
+
+echo '<div id="options_container">',
+     '<form action="index.php" method="post">',
+     '<select multiple="multiple" name="roles_ids[]" size="10" id="roles_ids">';
+
+foreach ($available_roles as $rid => $r) {
+    $selected = '';
+    if (!empty($roles_ids)) {
+        $selected = in_array($rid, $roles_ids) ? 'selected="selected"' : '';
+    }
+    echo "<option value=\"{$rid}\" {$selected}>{$r->name}</option>";
+}
+
+echo '</select>',
+     '<p>',
+         '<label for="repeat_each">', get_string('repeat_each', 'report_rolescapabilities'), '</label>',
+         '<input type="text" id="repeat_each" name="repeat_each" value="', $repeat_each, '" size="2" />',
+         '<input type="submit" value="', get_string('show'), '" />',
+     '</p>',
+     '</form>',
+     '</div>';
+
+if (empty($available_roles)) {
+    echo $OUTPUT->heading(get_string('no_roles_available', 'report_rolescapabilities'));
+}
+
 class rolescapabilities_table extends capability_table_base {
 
     public function __construct($context, $id, $roleids, $repeat_each) {
-        global $DB;
+        global $DB, $CFG;
 
         parent::__construct($context, $id);
 
@@ -64,11 +96,11 @@ class rolescapabilities_table extends capability_table_base {
     }
 
     protected function add_header_cells() {
-        $th = '<th>' . get_string('allowed', 'role') . '</th>';
+        $th = '';
         foreach ($this->roles as $rid => $r) {
             $th .= "<th class=\"role\">{$r->name}</th>";
         }
-        $th .= '<th class="name" align="left" scope="col">'.get_string('capability','role').'</th>';
+        $th .= '<th>'.get_string('risks').'</th>';
         echo $th;
     }
 
@@ -77,25 +109,28 @@ class rolescapabilities_table extends capability_table_base {
     }
 
     protected function add_row_cells($capability) {
-        $capabilities = array_chunk(get_moodle_capabilities($this->roles), $this->repeat_each);
-        $this->add_header_cells();
-        foreach ($capabilities as $chunk) {
+        global $DB;
 
-            foreach ($chunk as $capability) {
-
-                $cap_string = get_cap_string($capability);
-                echo '<tr>', $cap_string;
-                foreach ($roles as $role) {
-                    if (isset($capability[$role->shortname])) {
-                        echo '<td class="role cap_', $capability[$role->shortname] , '">';
-                    } else {
-                        echo '<td class="role cap_not_set">';
-                    }
-                    echo '</td>';
-                }   
-                echo $cap_string, '</tr>';
+        foreach ($this->roles as $role) {
+            $permission = $DB->get_records_menu('role_capabilities',
+                                                array('roleid' => $role->id,
+                                                     'contextid' => $this->context->id,
+                                                     'capability' => $capability->name),
+                                                '', 'capability,permission');
+            if ($permission) {
+                echo '<td class="role cap_', $permission[$capability->name] , '"></td>';
+            } else {
+                echo '<td class="role cap_not_set"></td>';
             }
         }
+        echo '<td>';
+    /// One cell for all possible risks.
+        foreach ($this->allrisks as $riskname => $risk) {
+            if ($risk & (int)$capability->riskbitmask) {
+               echo $this->get_risk_icon($riskname);
+            }
+        }
+        echo '</td>';
     }
 
     /**
@@ -117,88 +152,14 @@ class rolescapabilities_table extends capability_table_base {
 }
 
 
-
-$sql = "SELECT id, name 
-          FROM {$CFG->prefix}role
-         WHERE id IN ({$CFG->report_rolescapabilities_available_roles})
-      ORDER BY sortorder ASC";
-$available_roles = $DB->get_records_sql($sql);
-
-if (empty($available_roles)) {
-    echo $OUTPUT->heading(get_string('no_roles_available', 'report_rolescapabilities'));
-}
-
-echo '<div id="options_container">',
-     '<form action="index.php" method="post">',
-     '<select multiple="multiple" name="roles_ids[]" size="10" id="roles_ids">';
-
-foreach ($available_roles as $rid => $r) {
-    $selected = '';
-    if (!empty($roles_ids)) {
-        $selected = in_array($rid, $roles_ids) ? 'selected="selected"' : '';
-    }
-    echo "<option value=\"{$rid}\" {$selected}>{$r->name}</option>";
-}
-
-echo '</select>',
-     '<p>',
-         '<label for="repeat_each">', get_string('repeat_each', 'report_rolescapabilities'), '</label>',
-         '<input type="text" id="repeat_each" name="repeat_each" value="', $repeat_each, '" size="2" />',
-     '</p>',
-     '<input type="submit" value="', get_string('show'), '" />',
-     '</form>',
-     '</div>';
-
 if (empty($roles_ids)) {
     echo $OUTPUT->heading(get_string('no_roles_selected', 'report_rolescapabilities'));
 } else {
 
     $report = new rolescapabilities_table(get_context_instance(CONTEXT_SYSTEM), 0, $roles_ids, $repeat_each);
-
     $report->display();
-
-    echo '</table>';
 }
 
 echo $OUTPUT->footer();
-
-function get_moodle_capabilities($roles) {
-    global $CFG, $DB;
-
-    $sql = "SELECT id, name, component, contextlevel, riskbitmask
-              FROM {$CFG->prefix}capabilities
-             WHERE name NOT LIKE 'moodle/legacy%'
-          ORDER BY contextlevel, name";
-
-    // first, all capabilities
-    $records = $DB->get_records_sql($sql);
-    $capabilities = array();
-    foreach ($records as $cap) {
-        $capabilities[$cap->name] = array('component' => $cap->component,
-                                          'contextlevel' => $cap->contextlevel,
-                                          'riskbitmask' => $cap->riskbitmask,
-                                          'name' => $cap->name);
-    }
-
-    // now, the permissions by role
-    foreach ($roles as $role) {
-
-        $sql = "SELECT rc.capability, rc.permission
-                  FROM {$CFG->prefix}role_capabilities rc
-                  JOIN {$CFG->prefix}capabilities c
-                    ON c.name = rc.capability
-                 WHERE rc.contextid = 1
-                   AND rc.roleid = {$role->id}
-                   AND rc.capability NOT LIKE 'moodle/legacy%'
-              ORDER BY c.contextlevel,c.name";
-
-        $records = $DB->get_records_sql($sql);
-
-        foreach ($records as $capability) {
-            $capabilities[$capability->capability][$role->shortname] = $capability->permission;
-        }
-    }
-    return $capabilities;
-}
 
 ?>

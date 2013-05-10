6936188f5788df37ca3a188a861c1171b4d077ad
Rex Lorenzo <rex@oid.ucla.edu>
2013-05-10 15:58:45 -0700

    Renaming roles's to just roles' so that it is correct english grammar.

--- lang/en/report_rolescapabilities.php
+++ lang/en/report_rolescapabilities.php
@@ -16,7 +16,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Displays a color-coded view of roles's capabilities
+ * Displays a color-coded view of roles' capabilities
  *
  * @package    report
  * @subpackage rolescapabilities
@@ -28,7 +28,7 @@
 $string['config_available_roles'] = 'Roles that are shown in Roles\'s Capabilities report';
 $string['desc_available_roles'] = 'Choose which roles will be available to user that can see the report.';
 $string['legend_title'] = 'Color legends.';
-$string['no_roles_selected'] = 'No roles was selected. Please select at least one role to show capabilities.';
+$string['no_roles_selected'] = 'No roles were selected. Please select at least one role to show capabilities.';
 $string['repeat_each'] = 'Repeat the header each N entries';
 $string['rolescapabilities'] = 'Roles\'s capabilities';
 $string['rolescapabilities:view'] = 'View Admin Report Roles\'s capabilities';
